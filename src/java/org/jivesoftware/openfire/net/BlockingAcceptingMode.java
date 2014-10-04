cbfc8211ce0dce7ddb48dd2e5aece96b2ab81aab
idwanglu2010 <idwanglu2010@gmail.com>
2014-10-04 13:33:03 +0800

    Openfire bug ,释放socket 资源

--- src/java/org/jivesoftware/openfire/net/BlockingAcceptingMode.java
+++ src/java/org/jivesoftware/openfire/net/BlockingAcceptingMode.java
@@ -33,47 +33,57 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Accepts new socket connections and uses a thread for each new connection.
- *
+ * 
  * @author Gaston Dombiak
  */
 class BlockingAcceptingMode extends SocketAcceptingMode {
 
-	private static final Logger Log = LoggerFactory.getLogger(BlockingAcceptingMode.class);
+	private static final Logger Log = LoggerFactory
+			.getLogger(BlockingAcceptingMode.class);
 
-    protected BlockingAcceptingMode(ConnectionManager connManager, ServerPort serverPort,
-            InetAddress bindInterface) throws IOException {
-        super(connManager, serverPort);
-        serverSocket = new ServerSocket(serverPort.getPort(), -1, bindInterface);
-    }
+	protected BlockingAcceptingMode(ConnectionManager connManager,
+			ServerPort serverPort, InetAddress bindInterface)
+			throws IOException {
+		super(connManager, serverPort);
+		serverSocket = new ServerSocket(serverPort.getPort(), -1, bindInterface);
+	}
 
-    /**
-     * About as simple as it gets.  The thread spins around an accept
-     * call getting sockets and creating new reading threads for each new connection.
-     */
-    @Override
+	/**
+	 * About as simple as it gets. The thread spins around an accept call
+	 * getting sockets and creating new reading threads for each new connection.
+	 */
+	@Override
 	public void run() {
-        while (notTerminated) {
-            try {
-                Socket sock = serverSocket.accept();
-                if (sock != null) {
-                    Log.debug("Connect " + sock.toString());
-                    SocketReader reader =
-                            connManager.createSocketReader(sock, false, serverPort, true);
-                    Thread thread = new Thread(reader, reader.getName());
-                    thread.setDaemon(true);
-                    thread.setPriority(Thread.NORM_PRIORITY);
-                    thread.start();
-                }
-            }
-            catch (IOException ie) {
-                if (notTerminated) {
-                    Log.error(LocaleUtils.getLocalizedString("admin.error.accept"),
-                            ie);
-                }
-            }
-            catch (Throwable e) {
-                Log.error(LocaleUtils.getLocalizedString("admin.error.accept"), e);
-            }
-        }
-    }
+		while (notTerminated) {
+			Socket sock = null;
+			try {
+				sock = serverSocket.accept();
+				if (sock != null) {
+					Log.debug("Connect " + sock.toString());
+					SocketReader reader = connManager.createSocketReader(sock,
+							false, serverPort, true);
+					Thread thread = new Thread(reader, reader.getName());
+					thread.setDaemon(true);
+					thread.setPriority(Thread.NORM_PRIORITY);
+					thread.start();
+				}
+			} catch (IOException ie) {
+				if (notTerminated) {
+					Log.error(LocaleUtils
+							.getLocalizedString("admin.error.accept"), ie);
+				}
+			} catch (Throwable e) {
+				Log.error(LocaleUtils.getLocalizedString("admin.error.accept"),
+						e);
+			} finally {
+				if (sock != null) {
+					try {
+						sock.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
 }
